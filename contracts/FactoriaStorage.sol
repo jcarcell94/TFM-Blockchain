da018f259b65c2f0b36224547715e97743cc22c8
jcarcell94 <65658608+jcarcell94@users.noreply.github.com>
2020-09-07 20:57:15 +0200

    Update FactoriaStorage.sol
    - Añadido array de barcos
    - Añadido mappings (viaje -> owner, barco -> owner, IMO -> barco)
    - Añadido autoincremental Id usado en viajes
    - Añadida función validIMO para comtrolar que un IMO no esta usado

--- contracts/FactoriaStorage.sol
+++ contracts/FactoriaStorage.sol
@@ -1,13 +1,16 @@
 pragma solidity ^0.5.11;
 
-import './Libraries/Ownable.sol';
+import '../Libraries/Ownable.sol';
 
 contract FactoriaStorage is Ownable{
     
     address[] public viajes;
-    /// address[] public barcos;
-    address storageDir;
+    address[] public barcos;
     address factoryAddress;
+    mapping(address => address) viajeOwner;
+    mapping(address => address) barcoOwner;
+    mapping(string => address) IMOBarco;
+    uint id; /// Numerico autoincremental
     
     /// Evento para notificar un cambio de version en la factoria
     /// Indica cual era la direccion de la version anterior de la facotira y la nueva por la que se ha cambiado
@@ -23,29 +26,28 @@ contract FactoriaStorage is Ownable{
         _;
     }
     
-    function addViaje(address _viaje) onlyFactory public {
+    function addViaje(address _viaje, address _owner) onlyFactory external {
         viajes.push(_viaje);
+        viajeOwner[_viaje] = _owner;
     }
     
     function getViajes() onlyFactory public view returns(address[] memory){
         return viajes;
     }
     
-    /**function addBarco(address _barco) public {
+    function addBarco(address _barco, address _owner,string calldata _IMO) onlyFactory external {
         barcos.push(_barco);
+        barcoOwner[_barco] = _owner;
+        IMOBarco[_IMO] = _barco;
     }
     
-    function getBarcos() public view returns(address[] memory){
+    function getBarcos() onlyFactory public view returns(address[] memory){
         return barcos;
-    }**/
-
-    function getDir() public view returns(address _dir){
-        return address(this);
     }
     
     /// Cuando se quiere desplegar una nueva version de factoria 
     /// la version actual de facotry indica cual es la nueva version de factory
-    function changeFactoryVersion(address newFactory) public onlyFactory {
+    function changeFactoryVersion(address newFactory) external onlyFactory {
         _changeFactoryVersion(newFactory); 
     }
     
@@ -58,5 +60,16 @@ contract FactoriaStorage is Ownable{
     // Fallback function
     function() external payable{
         require(msg.value == 0, 'Este contrato no acepta Ether');
+    }
+    
+    /// Devulve un nuevo id para viaje
+    function newId() external returns(uint256) {
+        id += 1;
+        return id;
+    }
+    
+    function validIMO(string calldata _IMO) external view returns(bool) {
+        if(IMOBarco[_IMO] == address(0)) return false;
+        else return true;
     }
 }
