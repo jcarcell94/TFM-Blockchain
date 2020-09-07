9954f050230e571be7421f28a5d59952c4609dbf
jcarcell94 <65658608+jcarcell94@users.noreply.github.com>
2020-09-07 21:03:11 +0200

    Update FactoriaViaje.sol
    IMPORTANTE: Compila pero no se puede desplegar debido al tamaño de bloque
    - Corrección ruta de librerias
    - Añadidos eventos NewBarco,  NewViaje,  event EmergencyStop
    - Añadida función createBarco
    - Modificado función createViaje para controlar el acceso y emitir un evento.
    - Correción de algunos bugs

--- contracts/FactoriaViaje.sol
+++ contracts/FactoriaViaje.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.5.11;
 import './Barco.sol';
 import './Viaje.sol';
 import './FactoriaStorage.sol';
+import '../Libraries/Ownable.sol';
 
-contract FactoriaViaje {
+contract FactoriaViaje is Ownable{
    
    FactoriaStorage public factoriaStorage;
    bool paused;
+   
+   event NewBarco(address indexed _owner, address _barco);
+   event NewViaje(address indexed _owner, address _barco, address _vaije);
+   event EmergencyStop(bool _stop);
    
    constructor() public{
       factoriaStorage = new FactoriaStorage();
@@ -15,53 +20,52 @@ contract FactoriaViaje {
    
    modifier notPaused() {
        require(!paused,'Contract is paused');
+       _;
+   }
+   
+   // Crear un nuevo Barco
+   function createBarco(string memory _IMO, string memory _tipoEmbarcacion, uint _eslora, uint _manga, 
+    string memory _motor, uint _fechaBotadura, uint _numLicencia, string memory _puerto, uint _capacidadCarga) public{
+        require(factoriaStorage.validIMO(_IMO), 'IMO en uso.');
+        Barco newBarco = new Barco(_IMO, _tipoEmbarcacion, _eslora, _manga, _motor, _fechaBotadura, 
+                                    _numLicencia, _puerto, _capacidadCarga);
+        newBarco.transferOwnership(msg.sender);
+        factoriaStorage.addBarco(address(newBarco), msg.sender, _IMO);
+        emit NewBarco(msg.sender, address(newBarco));
    }
 
     // Crear un nuevo viaje y añadir la dirección
-    function createTravel( uint _ID, string memory _empresa, string memory _puertoIni, string memory _puertoFinalEst, string memory _proposito, address _barcoDir) notPaused onlyOwner public{
-        Viaje newViaje = new Viaje(_ID, _empresa, _puertoIni, _puertoFinalEst, _proposito, _barcoDir);
-        factoriaStorage.addViaje(address(newViaje));
+    function createTravel(string memory _empresa, string memory _puertoIni, string memory _puertoFinalEst, string memory _proposito, address _barcoDir) notPaused onlyOwner public{
+        require(Barco(_barcoDir).owner() == msg.sender,'No tiene permisos para crear el viaje o direccion de barco invalida');
+        Viaje newViaje = new Viaje(factoriaStorage.newId(), _empresa, _puertoIni, _puertoFinalEst, _proposito, _barcoDir);
+        newViaje.transferOwnership(msg.sender);
+        factoriaStorage.addViaje(address(newViaje), msg.sender);
+        emit NewViaje(msg.sender, _barcoDir, address(newViaje));
     }
 
+    // Devuelve una lista de las direcciones de los barcos
+    function getBarcos() public view returns (address[] memory){
+        return factoriaStorage.getBarcos();
+    }
+    
     // Devuelve una lista de las direcciones de los viajes
     function getViajes() public view returns (address[] memory){
         return factoriaStorage.getViajes();
     }
- 
-    // Devuelve la información del viaje
-    function getViaje(address viajeAddress) public view returns (uint _ID, string memory _puertoIni, string memory _proposito, string memory _estadoViaje, string memory _estadoBarco){
-        Viaje viaje = Viaje(viajeAddress);
-        return viaje.getViaje();
-    }
     
-    // Devuelve el estado de un viaje
-    function getStatusBarco(address viajeAddress) public view returns(string memory _estadoBarco){
-        Viaje viaje = Viaje(viajeAddress);
-        return viaje.getStatusBarco();
-    }
-
-    function getStatusViaje(address viajeAddress) public view returns(string memory _estadoViaje){
-        Viaje viaje = Viaje(viajeAddress);
-        return viaje.getStatusViaje();
-        }
-        
-    // Devuelve la última localización del barco
-    function getLocalization(address viajeAddress) public view returns(bytes32 _localizacion){
-        Viaje viaje = Viaje(viajeAddress);
-        return viaje.getLocalization();
-    }
-    
-    function changeFactoryVersion(address newFactory) notPaused onlyOwner public view {
+    function changeFactoryVersion(address newFactory) notPaused onlyOwner public {
         FactoriaStorage fs = FactoriaStorage(factoriaStorage);
         fs.changeFactoryVersion(newFactory);
     }
     
-    function emergencyStop() onlyOwner {
+    function emergencyStop() onlyOwner public{
         paused = true;
+        emit EmergencyStop(true);
     }
     
-    function resume() onlyOwner {
+    function resume() onlyOwner public{
         paused = false;
+        emit EmergencyStop(false);
     }
 
 }
